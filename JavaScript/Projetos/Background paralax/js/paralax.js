7d648669d3452d4075c0ff6f3ab7907296253c9f
João Bagatoli <joaovitorbagatoli07@gmail.com>
2024-02-07 14:21:38 -0300

    new project

---
+++ JavaScript/Projetos/Background paralax/js/paralax.js
@@ -0,0 +1,27 @@
+(function(){
+    window.addEventListener("scroll", positionImage)
+
+    const dataParalax = [...document.querySelectorAll("[data-paralax]")]
+
+    function isGettingOut(element){
+        return element.getBoundingClientRect().top <= 0
+    }
+
+    function getNewPosition(element){
+        const dataVelocity = parseFloat(element.getAttribute("data-p-velocity")) || .5
+        return element.getBoundingClientRect().top * dataVelocity * -1
+    }
+
+    function positionImage(){
+        dataParalax.forEach(element => {
+            let originalXPosition = getComputedStyle(element).backgroundPositionX
+            let originalYPosition = getComputedStyle(element).backgroundPositionY
+
+            if(isGettingOut(element)){
+                element.style.backgroundPosition = `${originalXPosition} ${getNewPosition(element)}px`
+            }
+        })
+    }
+
+    positionImage()
+})()
