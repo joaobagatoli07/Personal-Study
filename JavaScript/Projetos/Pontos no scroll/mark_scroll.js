6f3ee748c857df61ac7901840ac60a557e430b71
João Bagatoli <joaovitorbagatoli07@gmail.com>
2024-02-07 12:33:09 -0300

    update

--- JavaScript/Projetos/Pontos no scroll/mark_scroll.js
+++ JavaScript/Projetos/Pontos no scroll/mark_scroll.js
@@ -4,14 +4,32 @@
     const nav = document.querySelector(".nav")
     const links = nav.querySelectorAll("li a")
 
-
     function markScroll() {
         let positions = [...links].map(link => {
-            console.log(link)
-            console.log(link.href)
+            let href = link.getAttribute("href")
+            let h2 = document.querySelector(href)
+
+            return h2.getBoundingClientRect().top
         })
 
-        positions
+        let activeLink = getLastViewedElement(positions)
+
+        let activedNav = nav.querySelector(".actived")
+
+        if (activedNav) {
+            activedNav.classList.remove("actived")
+        }
+
+        if (activeLink) {
+            activeLink.classList.add("actived")
+        }else{
+            links[0].classList.add("actived")
+        }
+    }
+
+    function getLastViewedElement(ArrPositions) {
+        let _positions = ArrPositions.filter(n => n < innerHeight / 2)
+        return links[_positions.length - 1]
     }
 
     markScroll()
