1f9a4fbafc87e79916c8251b52ce2d84bb8d62e9
João Bagatoli <joaovitorbagatoli07@gmail.com>
2024-02-21 14:43:29 -0300

    update

--- JavaScript/Projetos/Conta Bancária/conta.js
+++ JavaScript/Projetos/Conta Bancária/conta.js
@@ -11,14 +11,14 @@ class Cliente {
     }
 }
 
-class PessoaFisica extends Cliente{
-    constructor(nome, documento){
+class PessoaFisica extends Cliente {
+    constructor(nome, documento) {
         super(nome, documento, "CPF")
     }
 }
 
-class PessoaJuridica extends Cliente{
-    constructor(nome, documento){
+class PessoaJuridica extends Cliente {
+    constructor(nome, documento) {
         super(nome, documento, "CNPJ")
     }
 }
@@ -35,7 +35,7 @@ class ContaBancaria {
         this.saldo = 0
     }
 
-    get dadosCliente(){
+    get dadosCliente() {
         return `${this.cliente.nome}, ${this.cliente.tipoDocumento}: ${this.cliente.documento}`
     }
 
@@ -79,6 +79,22 @@ class ContaPoupanca extends ContaBancaria {
     }
 }
 
+class Transferir {
+    static execute(contaOrigem, contaDestino, valor) {
+        if (!contaOrigem instanceof ContaBancaria || 
+            !contaDestino instanceof ContaBancaria){
+                throw Error("Contas precisam herdar de ContaBancaria")
+        }
+
+        try{
+            contaOrigem.sacar(valor)
+            contaDestino.depositar(valor)
+        }catch(e){
+            throw Error("Algo deu errado...", e.message)
+        }
+    }
+}
+
 // TESTES
 
 const joao = new PessoaFisica("João", "134.091.360-11")
@@ -86,3 +102,11 @@ const kidsGraca = new PessoaJuridica("KidsGraca", "122.133.144/0001-01")
 
 const cc = new ContaCorrente(joao, 7)
 const cp = new ContaPoupanca(kidsGraca, 2)
+
+cc.depositar(3000)
+cc.limite = 1000
+
+cp.depositar(2000)
+cp.sacar(800)
+
+Transferir.execute(cp, cc, 500)
