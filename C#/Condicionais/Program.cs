8d1793c60d9bf126f7eb844de279384741172a77
João Bagatoli <joaovitorbagatoli07@gmail.com>
2024-03-29 11:52:20 -0300

    new

---
+++ C#/Condicionais/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Condicionais;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int idade = 20;
+        var maiorIdade = 18;
+        bool temDocumento = false;
+
+        if (25 != 32) Console.WriteLine("É diferente");
+
+        if (idade >= maiorIdade && temDocumento)
+            Console.WriteLine("Pode entrar");
+        else
+            Console.WriteLine("Não pode entrar");
+    }
+}
