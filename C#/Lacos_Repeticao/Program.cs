a8e58c47ac2fb577148d21806c17a82554dd929e
João Vitor Bagatoli <joaovitorbagatoli07@gmail.com>
2024-04-01 18:15:50 -0300

    update

--- C#/Lacos_Repeticao/Program.cs
+++ C#/Lacos_Repeticao/Program.cs
@@ -5,9 +5,24 @@ class Program
 {
     static void Main(string[] args)
     {
+        int valor = 3;
+
+        Console.WriteLine("For");
         for (int i = 0; i <= 5; i++)
-        {
             Console.WriteLine(i);
+
+        Console.WriteLine("While");
+        while (valor <= 5)
+        {
+            Console.WriteLine(valor);
+            valor++;
         }
+
+        Console.WriteLine("Do while");
+        do
+        {
+            Console.WriteLine(valor);
+            valor++;
+        } while (valor < 5);
     }
 }
--- C#/Lacos_Repeticao/obj/Debug/net8.0/Lacos_Repeticao.AssemblyInfo.cs
+++ C#/Lacos_Repeticao/obj/Debug/net8.0/Lacos_Repeticao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lacos_Repeticao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8d3594f5c183d95bc32980b427662ec31a320c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11effc5dbbb872dbd59e4290e073f91ec5725d63")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lacos_Repeticao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lacos_Repeticao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
