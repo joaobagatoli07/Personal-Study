11effc5dbbb872dbd59e4290e073f91ec5725d63
João Vitor Bagatoli <joaovitorbagatoli07@gmail.com>
2024-04-01 18:01:22 -0300

    update

--- C#/Lacos_Repeticao/Program.cs
+++ C#/Lacos_Repeticao/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LacosRepeticao;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
