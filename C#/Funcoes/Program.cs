313f0a4e3fac33fef08964b9b05cd2f32b8a89e1
João Vitor Bagatoli <joaovitorbagatoli07@gmail.com>
2024-04-03 15:28:39 -0300

    update

--- C#/Funcoes/Program.cs
+++ C#/Funcoes/Program.cs
@@ -5,6 +5,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        MeuMetodo();
+        string nome = Nome("João", "Bagatoli");
+        Console.WriteLine(nome);
+    }
+
+    static void MeuMetodo()
+    {
+        Console.WriteLine("C# é legal");
+    }
+
+    static string Nome(string nome, string sobrenome, int idade)
+    {
+        return nome + " " + sobrenome;
     }
 }
