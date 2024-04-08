2e8edfb2ccee1b98ffcc72981d176ccde6bb41ec
João Vitor Bagatoli <joaovitorbagatoli07@gmail.com>
2024-04-08 15:52:30 -0300

    new

--- C#/Valores_Referencia/Program.cs
+++ C#/Valores_Referencia/Program.cs
@@ -5,6 +5,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        int x = 25;
+        int y = x;
+
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+
+        x = 32;
+
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+
+        var arr = new string[2];
+        arr[0] = "Item 1";
+
+        var arr2 = arr;
+
+        Console.WriteLine(arr[0]);
+        Console.WriteLine(arr2[0]);
+
+        arr[0] = "Item alterado";
+
+        Console.WriteLine(arr[0]);
+        Console.WriteLine(arr2[0]);
     }
 }
