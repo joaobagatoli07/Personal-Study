96785991f8935cffff334f1ad1cb8d601ba32c9d
João Vitor Bagatoli <joaovitorbagatoli07@gmail.com>
2024-04-10 13:55:21 -0300

    new

---
+++ C#/Projetos/Cronometro/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("--- CRONOMETRO ---");
+
+            Console.WriteLine("Escolha a métrica");
+            Console.WriteLine("S = Segundos");
+            Console.WriteLine("M = Minutos");
+            string metrica = Console.ReadLine();
+
+            Console.WriteLine("Quanto tempo você deseja contar?");
+            int time = int.Parse(Console.ReadLine());
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000); // a execução fica em espera por 1s
+            }
+
+            Console.Clear();
+            Console.WriteLine("Cronometro finalizado");
+            Thread.Sleep(2500);
+        }
+    }
+}
