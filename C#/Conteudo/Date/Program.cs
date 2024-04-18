8b1c2ec233f60d771b525048cb16d05aead60957
João Vitor Bagatoli <joaovitorbagatoli07@gmail.com>
2024-04-18 18:26:45 -0300

    new

---
+++ C#/Conteudo/Date/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Date
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new DateTime(2005, 12, 7); 
+            var dataAtual = DateTime.Now;
+            Console.WriteLine(data);
+            Console.WriteLine(dataAtual);
+            Console.WriteLine(dataAtual.Year);
+        }
+    }
+}
