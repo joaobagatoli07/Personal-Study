3845ec65e38e5b4320902d70696dfcc0cf79183a
João Vitor Bagatoli <joaovitorbagatoli07@gmail.com>
2024-04-22 13:04:59 -0300

    new

---
+++ C#/Conteudo/Moedas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Currency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal valor = 10.25m;
+            Console.WriteLine(valor.ToString(
+                "C",
+                CultureInfo.CreateSpecificCulture("pt-br")));
+
+            Console.WriteLine(Math.Round(valor));
+            Console.WriteLine(Math.Ceiling(valor));
+            Console.WriteLine(Math.Floor(valor));
+        }
+    }
+}
