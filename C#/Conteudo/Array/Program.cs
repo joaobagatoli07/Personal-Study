3dcdeff94ee2d2caded5dd6740a01cc3f3dc4959
João Vitor Bagatoli <joaovitorbagatoli07@gmail.com>
2024-04-22 18:27:44 -0300

    new

---
+++ C#/Conteudo/Array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Array
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var arr = new int[5] { 1, 2, 3, 4, 5 };
+            arr[0] = 12;
+
+            // for (int i = 0; i < arr.Length; i++)
+            // {
+            //     Console.WriteLine(arr[i]);
+            // }
+
+            foreach (var item in arr)
+               Console.WriteLine(item);
+              
+        }
+    }
+}
